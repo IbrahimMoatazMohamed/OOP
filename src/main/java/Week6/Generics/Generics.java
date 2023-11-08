1fa8a1629bcacf717fdf5d698f480a56c30c7727
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2023-11-09 00:50:37 +0100

    Week6

--- src/main/java/Week6/Generics/Generics.java
+++ src/main/java/Week6/Generics/Generics.java
@@ -1,5 +1,43 @@
 package Week6.Generics;
 
-public class Generics {
+import java.util.*;
 
+public class Generics {
+    public static <T> void sumOddAndEven(List<T> nums){
+        int even = 0, odd = 0;
+        for(T i: nums){
+            if(i instanceof Integer){
+                if((int)i % 2 == 0){
+                    even += (int) i;
+                }else {
+                    odd += (int) i;
+                }
+            } else if (i instanceof Double) {
+                double j = ((Double) i).doubleValue();
+                int k = (int) j;
+                if(k % 2 == 0){
+                    even += k;
+                }else {
+                    odd += k;
+                }
+            }else{
+                float j = ((Float) i).floatValue();
+                int k = (int) j;
+                if(k % 2 == 0){
+                    even += k;
+                }else {
+                    odd += k;
+                }
+            }
+        }
+        System.out.println("The sum of odd: " + odd + "\nThe sum of even: " + even);
+    }
+    public static <T> void reverse(List<T> list){
+        T temp1;
+        for(int i = 0, j = list.size() - 1; i < j; i++, j--){
+            temp1 = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp1);
+        }
+    }
 }
--- src/main/java/Week6/Generics/Main.java
+++ src/main/java/Week6/Generics/Main.java
@@ -1,7 +1,23 @@
 package Week6.Generics;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Main {
+    public static <T> void print(List<T> list){
+        for(T i : list){
+            System.out.print(i + "  ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        List<Double> list = new ArrayList<>();
+        list.add(234.4);
+        list.add(234.4);
+        list.add(123.4);
+        list.add(2331.);
+        list.add(290.);
+        print(list);
+        Generics.reverse(list);
+        print(list);
     }
 }
