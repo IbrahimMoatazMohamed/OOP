7662c9addf222f5033231a7b4f73a09280c604f3
IbrahimMoatazMohamed <146713911+IbrahimMoatazMohamed@users.noreply.github.com>
2023-12-22 14:36:35 +0100

    Delete

--- src/main/java/Lab10/ChatApplication.java
+++
@@ -1,9 +0,0 @@
-package Lab10;
-
-public class ChatApplication {
-    public static void main(String[] args) {
-        ChatServer chatServer = new ChatServer();
-        chatServer.start(8888);
-    }
-}
-
